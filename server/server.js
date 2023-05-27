6b64de021a21ca75890b5cf72565a0185f669e20
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-05-27 17:30:00 -0500

    defined a connection to the database using mysql

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import mysql from 'mysql';
+// const express = require('express');
+const app = express();
+import  express  from 'express';
+
+
+ export default db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'furniture_shop'
+});
+
