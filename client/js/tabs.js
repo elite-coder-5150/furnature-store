77f84e43a78f5b163681646848f76c284585e9ed
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-06-02 10:09:41 -0500

    cmpiled typescript

--- client/js/tabs.js
+++ client/js/tabs.js
@@ -10,3 +10,8 @@ tabs.forEach(function (tab, index) {
         tabContents[index].classList.add('active');
     });
 });
+// q: what does the above code do?
+// a: it adds an event listener to each tab, so that when a tab is clicked,
+//    it will remove the 'active' class from all tabs and tab contents, and
+//    then add the 'active' class to the clicked tab and corresponding tab
+//    content
